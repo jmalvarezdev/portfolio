d99a5335bbd253da78e4b437bcc519d1f2836d1b
Jose Alvarez <logan_3x@hotmail.com>
2018-03-16 10:14:32 -0400

    Pull from other GitHub account I have

--- index.php
+++ index.php
@@ -24,9 +24,9 @@
     <body>
 
     <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <span class="navbar-brand">
-        <img class="img-fluid" src="images/logo.png" width="80" height="80" alt="logo">
+        <img class="img-fluid" src="images/logo.png" width="100" height="100" alt="logo">
         </span>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -37,6 +37,7 @@
             <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
             <li class="nav-item"><a class="nav-link" href="#">About</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Skills</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Portfolio</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Social</a></li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -67,15 +68,15 @@
                 <img class="d-block w-100" src="images/Boston.jpg" alt="Cute pet">
                 <div class="carousel-caption d-none d-md-block">
                     <h1 class="car-heading">I love Boston!</h1>
-                    <p>I really enjoy exploring this beautiful city.</p>
+                    <p class="car-par">I really enjoy exploring this beautiful city.</p>
                 </div>
             </div>
 
              <div class="carousel-item">
                 <img class="d-block w-100" src="images/night.jpg" alt="Cute pet">
                 <div class="carousel-caption d-none d-md-block">
-                    <h1 class="car-heading">My View</h1>
-                    <p>The view from my balcony is amazing and relaxing.</p>
+                    <h1 class="car-heading">My neighborhood</h1>
+                    <p class="car-par">The scenery is at its best during the night.</p>
                 </div>
             </div>
 
@@ -83,7 +84,7 @@
                 <img class="d-block w-100" src="images/code.jpg" alt="Cute pet">
                 <div class="carousel-caption d-none d-md-block">
                     <h1 class="car-heading">I love learning!</h1>
-                    <p>I am a self taught developer.</p>
+                    <p class="car-par">I am a self taught developer.</p>
                 </div>
             </div>
 
@@ -101,6 +102,7 @@
     </div> <!-- End of my carousel -->
 </div> <!-- End of my carousel container -->
 
+
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
@@ -116,13 +118,14 @@
                     <p>a Video Editor.</p>
                     <p>a Food lover.</p>
                 </div>
+<hr>
             </div>
         </div>
     </div>
 </div>
 
 <div class="container-fluid">
-    <section class="row">
+    <section class="row align-items-center">
         <div class="col-lg-6 col-md-6">
             <img class="img-fluid" src="images/me.png" alt="Author">
         </div>
@@ -138,62 +141,196 @@ But overall im just a simple person doing what he loves.</p>
     </section>
 </div>
 
-<div class="container-fluid">
+<div class="container-fluid back-back">
     <h2 class="skills">Skills</h2>
     
-    <div class="row">
-        <div class="col-md-4 animated fadeInDown">
-            <span class="fa fa-code"></span>
-            <h3>Web Desing & Code</h3>
-            <p>Simple, usable, attractive, standards compliant & responsive websites you can rely on.
+    <div class="row back-back">
+        <div class="col-md-4">
+            <span class="fa fa-code fa-7x"></span>
+            <h3 class="skill-header">Web Desing & Code</h3>
+            <p class="skill-par">Simple, usable, attractive, standards compliant & responsive websites you can rely on.
                                         The web development process includes web design, web content development, client-side/server-side
                                         scripting and network security configuration, among other tasks.</p>
         </div>
         <div class="col-md-4">
-            <span class="fab fa-css3-alt"></span>
-            <h3>CSS3</h3>
-            <p>CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.1. It brings a lot of long-awaited novelties, like rounded corners, shadows, gradients, transitions or animations, as well as new layouts like multi-columns, flexible box or grid layouts.<p>
+            <span class="fab fa-css3-alt fa-7x"></span>
+            <h3 class="skill-header">CSS3</h3>
+            <p class="skill-par">CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.1. It brings a lot of long-awaited novelties, like rounded corners, shadows, gradients, transitions or animations, as well as new layouts like multi-columns, flexible box or grid layouts.<p>
         </div>
         <div class="col-md-4">
             <i class="devicon-vim-plain colored"></i>
-            <h3>Vim</h3>
-            <p>Vim is a clone of Bill Joy's vi text editor program for Unix. It was written by Bram Moolenaar based on source for a port of the Stevie editor to the Amiga and first released publicly in 1991. I love the ability to tweak my tools depending on my environment and Vim does that perfectly.<p>
+            <h3 class="skill-header">Vim</h3>
+            <p class="skill-par">Vim is a clone of Bill Joy's vi text editor program for Unix. It was written by Bram Moolenaar based on source for a port of the Stevie editor to the Amiga and first released publicly in 1991. I love the ability to tweak my tools depending on my environment and Vim does that perfectly.<p>
+        </div>
+        <div class="col-md-4">
+            <span class="devicon-javascript-plain colored"></span>
+            <h3 class="skill-header">Javascript</h3>
+            <p class="skill-par">
+                JavaScript, often abbreviated as JS, is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm. 
+            </p>
+
+        </div>
+        <div class="col-md-4">
+            <span class="devicon-php-plain colored"></span>
+            <h3 class="skill-header">PHP</h3>
+            <p class="skill-par">
+                PHP is a server-side scripting language designed for web development but also used as a general-purpose programming language. Originally created by Rasmus Lerdorf in 1994, the PHP reference implementation is now produced by The PHP Group.
+            </p>
+
+
+        </div>
+        <div class="col-md-4">
+            <span class="devicon-laravel-plain colored"></span>
+            <h3 class="skill-header">Laravel</h3>
+            <p class="skill-par">
+                Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern. 
+            </p>
+
+
         </div>
     </div>
 </div>
 
-<div class="container-fluid">
+<div id="map"></div>
+
+<div class="container-fluid back-back">
+    <h1 class="contact">Portfolio</h1>
     <div class="row">
-        <div class="col-md-12">
-            <h1>Contact me</h1>
-            <h2>Want to chat? Get in touch!<h2>
-            <p>Feel free to connect via any social network or email me at: 
-            <br> 
-            <a href="mailto:logan.jma@gmail.com">Email me</a></p>
-            <a href="#"><i class="fab fa-instagram"></i></a>
-            <a href="#"><i class="fab fa-twitter-square"></i></a>
-            <a href="#"><i class="fab fa-linkedin"></i></a>
-            <a href="#"><i class="fab fa-github-square"></i></a>
-            <a href="#"><i class="fab fa-chrome"></i></a>
-            <a href="#"><i class="fab fa-youtube"></i></a>
-            <a href="#"><i class="fab fa-facebook-square"></i></a>
+        <div class="col-md-4 p-3">
+            <div class="card text-white bg-danger">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go Somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 p-3">
+            <div class="card text-white bg-success">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go Somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 p-3">
+            <div class="card">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go Somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 p-3">
+            <div class="card">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go Somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 p-3">
+            <div class="card">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go Somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 p-3">
+            <div class="card">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go Somewhere</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 
-<footer>
+<footer class="footer bg-dark">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-4 text-white">
                 <h1>Think Big, because we become what we think of</h1>
                 <p> &copy; 2018 by Jose Alvarez</p>
+            </div>
+            <div class="col-md-4 text-white">
+
+                <h2 class="contact-chat">Want to chat? Get in touch!<h2>
+
+                <div class="social-media">
+                <a href="#"><i style="color: white;" class="fab fa-instagram fa-lg"></i></a>
+                <a href="#"><i style="color: white;" class="fab fa-twitter-square fa-lg"></i></a>
+                <a href="#"><i style="color: white;" class="fab fa-linkedin fa-lg"></i></a>
+                <a href="#"><i style="color: white;" class="fab fa-github-square fa-lg"></i></a>
+                <a href="#"><i style="color: white;" class="fab fa-chrome fa-lg"></i></a>
+                <a href="#"><i style="color: white;" class="fab fa-youtube fa-lg"></i></a>
+                <a href="#"><i style="color: white;" class="fab fa-facebook-square fa-lg"></i></a>
+                </div>
+                   
+            </div>
+            <div class="col-md-4 text-white">
+             <section class="contact-info">
+            <h1 class="contact">Contact me</h1>
+
+            <div class="contact-email">
+            <a href="mailto:logan.jma@gmail.com">Email me</a></p>
+            </div>
+            </section>
+
             </div>
         </div>
     </div>
 </footer>
 
 
-            <script src="js/scripts.js"></script>
+            <script>
+                
+                  // The following example creates a marker in Stockholm, Sweden using a DROP
+                  // animation. Clicking on the marker will toggle the animation between a BOUNCE
+                  // animation and no animation.
+
+                  var marker;
+
+                  function initMap() {
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                      zoom: 13,
+                      center: {lat: 42.360082, lng: -71.058880}
+                    });
+
+                    marker = new google.maps.Marker({
+                      map: map,
+                      draggable: true,
+                      animation: google.maps.Animation.DROP,
+                      position: {lat: 42.360082, lng: -71.058880}
+                    });
+                    marker.addListener('click', toggleBounce);
+                  }
+
+                  function toggleBounce() {
+                    if (marker.getAnimation() !== null) {
+                      marker.setAnimation(null);
+                    } else {
+                      marker.setAnimation(google.maps.Animation.BOUNCE);
+                    }
+                  }
+
+            </script>
+            <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY8k&callback=initMap">
+            </script>
             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
